eedc39c99aa98ade3d2afaf0ad7328533855a897
Mirza Brcko <mirza.arnautovic@outlook.com>
2020-09-03 16:19:38 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function menuToggle() {
+  var nav = document.getElementById("menu-overlay");
+  nav.classList.toggle("active");
+  var nav1 = document.getElementById("toggle");
+  nav1.classList.toggle("active");
+}
